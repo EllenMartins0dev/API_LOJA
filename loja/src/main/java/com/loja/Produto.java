c12aaa4fc4fc09c372ae8388723bb7eb02fa9907
ellen <ellenmartins@aluno.senai.br>
2023-08-31 16:01:21 -0300

    Criando classes para produto e tabela de produtos :)

---
+++ loja/src/main/java/com/loja/Produto.java
@@ -0,0 +1,58 @@
+package com.loja;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double valorUnitario;
+
+    public Produto(int id, String nome, String descricao, double valorUnitario) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", valorUnitario=" + valorUnitario +
+                '}';
+    }
+}
---
+++ loja/src/main/java/com/loja/TabelaProdutos.java
@@ -0,0 +1,25 @@
+package com.loja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabelaProdutos {
+    List<Produto> produtos = new ArrayList<>();
+
+    // No construtor, a lista de produtos será preenchiada :)
+
+    public TabelaProdutos() {
+        Produto caneta = new Produto(1, "Caneta BIC", "Caneta BIC na cor azul", 1.99);
+        Produto borracha = new Produto(2, "Borracha branca", "Borracha branca comum", 3.99);
+        Produto lapis = new Produto(3, "Lápis Faber Castell", "Lápis Faber Castell comum", 2.99);
+        Produto apontador = new Produto(4, "Apontador", "Apontador comum com reservatório", 7.99);
+        Produto caderno = new Produto(5, "Caderno espiral", "Caderno espiral de 96 folhas", 15.99);
+
+        this.produtos.add(caneta);
+        this.produtos.add(borracha);
+        this.produtos.add(lapis);
+        this.produtos.add(apontador);
+        this.produtos.add(caderno);
+    }
+
+}
